24f90b7ee153336c4104025b68bb4bbc5595c2ce
laa0304 <endo.rodriguez@externos-co.cencosud.com>
2024-03-04 23:54:55 -0500

    Creacion de la estructura

---
+++ back/fullcell/src/main/java/com/endorodrigo/fullcell/FullcellApplication.java
@@ -0,0 +1,13 @@
+package com.endorodrigo.fullcell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullcellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullcellApplication.class, args);
+	}
+
+}

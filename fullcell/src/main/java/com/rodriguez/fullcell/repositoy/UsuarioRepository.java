26be38861fe4efb6603db04d54a90a5a2222e93d
laa0304 <endo.rodriguez@externos-co.cencosud.com>
2024-03-18 21:23:13 -0500

    Solucion de conflictos

--- fullcell/src/main/java/com/rodriguez/fullcell/repositoy/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rodriguez.fullcell.repositoy;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.rodriguez.fullcell.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-
-}

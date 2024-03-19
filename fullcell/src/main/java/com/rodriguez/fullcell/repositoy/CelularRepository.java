560b33df21282da888ab44895d900e2d447ed930
Endo Rodrigo Rodriguez Ariza <59849769+EndoRodrigo@users.noreply.github.com>
2024-03-19 12:45:47 -0500

    Delete fullcell directory

--- fullcell/src/main/java/com/rodriguez/fullcell/repositoy/CelularRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rodriguez.fullcell.repositoy;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.rodriguez.fullcell.model.Celular;
-
-public interface CelularRepository extends JpaRepository<Celular, Long>{
-
-}
--- fullcell/src/main/java/com/rodriguez/fullcell/repositoy/VentasRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rodriguez.fullcell.repositoy;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.rodriguez.fullcell.model.Ventas;
-
-public interface VentasRepository extends CrudRepository<Ventas, Long>{
-
-}
